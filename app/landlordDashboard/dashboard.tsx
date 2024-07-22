3e3c579c92ebc93e9a31c866dd6ef263173ee04a
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-23 02:24:51 +0300

    Add basic landlord dashboard

---
+++ app/landlordDashboard/dashboard.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import { router } from "expo-router";
+import React from "react";
+
+import Button from "../../components/common/Button";
+
+const Index = () => {
+  return (
+    <View>
+      <Button onPress={() => router.push("landlordDashboard/ChooseProperty")} title="Choose Property" />
+
+      <Button onPress={() => router.push("landlordDashboard/PropertyDetails")} title="Property Details" />
+    </View>
+  );
+};
+
+export default Index;

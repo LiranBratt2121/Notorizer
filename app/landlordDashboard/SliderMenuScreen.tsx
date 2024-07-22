1cdea438ffa05e3cc8bd798866cf7525076e2dbe
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-23 02:25:17 +0300

    Add slider menu wrapper

---
+++ app/landlordDashboard/SliderMenuScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SliderMenu from '../../components/common/SliderMenu';
+
+const SliderMenuScreen = () => {
+  return (
+    <SliderMenu />
+  );
+};
+
+export default SliderMenuScreen;

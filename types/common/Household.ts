9e5ca617a55e473a5d3ca36dabcb2b40ed03f110
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-26 22:43:15 +0300

    Add general house types

---
+++ types/common/Household.ts
@@ -0,0 +1,12 @@
+export type Apartment = {
+    addr?: string;
+    city?: string;
+    state?: string;
+    landlord?: string;
+    tenant?: string;
+  }
+
+export type Room = {
+  name?: string;
+  side?: number;
+}

a142663d8735a5e7b0ebf27dd7ddc990dc6e2372
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-27 18:28:22 +0300

    Add a basic choose property component

--- app/landlordDashboard/ChooseProperty.tsx
+++ app/landlordDashboard/ChooseProperty.tsx
@@ -1,74 +1,31 @@
-import React, { useState } from "react";
-import { View, StyleSheet, ScrollView, Text } from "react-native";
-import Camera from "@/components/common/Camera";
-import { Apartment, Room } from "../../types/common/Household";
-import * as FileSystem from "expo-file-system";
-import { SvgXml } from "react-native-svg";
-import base64 from 'react-native-base64';
-import Button from "@/components/common/Button";
+import { SafeAreaView, FlatList, StyleSheet } from 'react-native'
+import { useRouter } from 'expo-router'
+import React from 'react'
+import PropertyCard from '@/components/common/PropertyCard'
 
 const ChooseProperty = () => {
-  const [capturedImage, setCapturedImage] = useState<string>('');
-  const [showCamera, setShowCamera] = useState(true);
-
-  const apartment: Apartment = {
-    addr: "123 Main St",
-    city: "Springfield",
-    state: "IL",
-    landlord: "John Doe",
-    tenant: "Jane Smith",
-  };
-
-  const room: Room = {
-    name: "Living Room",
-    side: 1,
-  };
-
-  const handleImageCaptured = async (filePath: string) => {
-    const base64Image = await FileSystem.readAsStringAsync(filePath, {
-      encoding: FileSystem.EncodingType.Base64,
-    });
-    const svgString = base64.decode(base64Image);
-    setCapturedImage(svgString);
-    setShowCamera(false);
-  };
+  const data = [
+    <PropertyCard property={'Karen'} imageUrl={'XXXX'} onPress={() => {}}/>,
+    <PropertyCard property={'JOE'} imageUrl={'XXXX'} onPress={() => {}}/>,
+    <PropertyCard property={'LIBBY'} imageUrl={'XXXX'} onPress={() => {}}/>
+  ]    
 
   return (
-    <View style={styles.container}>
-      {showCamera ? (
-        <Camera
-          apartment={apartment}
-          room={room}
-          onImageCaptured={handleImageCaptured}
-        />
-      ) : (
-        <ScrollView contentContainerStyle={styles.imageContainer}>
-          {capturedImage ? (
-            <SvgXml xml={capturedImage} width="100%" height="300" />
-          ) : (
-            <Text>Loading...</Text>
-          )}
-          <Button title="Retake" onPress={() => setShowCamera(true)} />
-        </ScrollView>
-      )}
-    </View>
-  );
-};
+    <SafeAreaView>
+      <FlatList
+      data={data}
+      renderItem={({ item }) => item}
+      />
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: '#f5f5f5',
   },
-  imageContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  imagePreview: {
-    width: "100%",
-    height: 300,
-    resizeMode: "contain",
-    marginTop: 10,
-  },
-});
-
-export default ChooseProperty;
+})
+export default ChooseProperty

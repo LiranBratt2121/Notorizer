6d12a7eb3f583188f515dd31db309a2742d18d0e
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-10 02:40:08 +0300

    Modify uploadBase64Image to work with blobs

--- utils/StorageUtils.ts
+++ utils/StorageUtils.ts
@@ -1,15 +1,26 @@
-import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { getDownloadURL, ref, uploadBytes, uploadString } from "firebase/storage";
 import { storage } from "../firebaseConfig";
+import CryptoJS from 'crypto-js';
 
-const uploadBase64Image = async (base64String: string, fileName: string) => {
+// Function to generate a unique file name
+const generateUniqueFileName = (base64String: string): string => {
+  const hash = CryptoJS.MD5(base64String).toString();
+  return `images/${hash}.png`; // Currently works only on png
+};
+
+const uploadBase64Image = async (base64String: string): Promise<string> => {
   try {
-    const storageRef = ref(storage, `images/${fileName}`);
+    const fileName = generateUniqueFileName(base64String);
+    const storageRef = ref(storage, fileName);
 
-    const snapshot = await uploadString(storageRef, base64String, 'base64');
+    const base64Data = base64String.replace(/^data:image\/(png|jpeg);base64,/, '');
+    const response = await fetch(`data:image/png;base64,${base64Data}`);
+    const blob = await response.blob();
 
+    const snapshot = await uploadBytes(storageRef, blob);
     const downloadURL = await getDownloadURL(snapshot.ref);
 
-    console.log('Uploaded a Base64 string image!');
+    console.log('Uploaded a Base64 image as Blob!');
     console.log('File available at:', downloadURL);
 
     return downloadURL;
@@ -19,11 +30,8 @@ const uploadBase64Image = async (base64String: string, fileName: string) => {
   }
 };
 
-const fetchImageAsBase64 = async (fileName: string): Promise<string | null> => {
+const fetchImageAsBase64 = async (downloadURL: string): Promise<string | null> => {
   try {
-    const storageRef = ref(storage, `images/${fileName}`);
-    const downloadURL = await getDownloadURL(storageRef);
-
     const response = await fetch(downloadURL);
     const blob = await response.blob();
 

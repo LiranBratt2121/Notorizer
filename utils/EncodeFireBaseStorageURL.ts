72687dbdd78674cef5f9ebf1964044d751c64f2b
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-10 22:55:51 +0300

    Add a function for encoding LandlordVerificationData

--- utils/EncodeFireBaseStorageURL.ts
+++ utils/EncodeFireBaseStorageURL.ts
@@ -1,13 +1,26 @@
-function encodePath(url: string) {
+import { LandlordVerificationData, Property } from "@/types/common/Household";
+
+const encodePath = (url: string) => {
     const queryIndex = url.indexOf('?alt=media');
-  
+
     const path = url.substring(0, queryIndex);
-    
+
     const encodedPath = path.replace(/\/images\//g, '/images%2F');
-    
+
     const resultUrl = encodedPath + url.substring(queryIndex);
-    
+
     return resultUrl;
+}
+
+export const encodeLandlordVerificationData = (property: Property) => {
+    const urls = property.data.landlordVerificationData;
+
+    const encodedProperty: LandlordVerificationData = {
+        houseImageUrl: urls.houseImageUrl ? encodePath(urls.houseImageUrl) : null,
+        idImageUrl: urls.idImageUrl ? encodePath(urls.idImageUrl) : null,
+        ownershipImageUrl: urls.ownershipImageUrl ? encodePath(urls.ownershipImageUrl) : null,
+    }
+    return encodedProperty;
 }
 
 export default encodePath;

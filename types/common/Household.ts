27dd7d195eac81c3746d95d565450b177703a4c5
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-10 22:55:08 +0300

    Move types to general location

--- types/common/Household.ts
+++ types/common/Household.ts
@@ -1,12 +1,40 @@
 export type Apartment = {
-    addr?: string;
-    city?: string;
-    state?: string;
-    landlord?: string;
-    tenant?: string;
-  }
+  addr?: string;
+  city?: string;
+  state?: string;
+  landlord?: string;
+  tenant?: string;
+}
 
 export type Room = {
   name?: string;
   side?: number;
+}
+
+export type LandlordVerificationData = {
+  idImageUrl: string | null;
+  ownershipImageUrl: string | null;
+  houseImageUrl: string | null;
+};
+
+export interface RoomData {
+  images?: string[];
+  name?: string;
+}
+
+export type Data = {
+  addExternalSpace?: RoomData
+  addRooms?: RoomData
+  bedrooms?: RoomData
+  externalView?: RoomData
+  kitchen?: RoomData
+  landlordVerificationData: LandlordVerificationData
+  livingRooms?: RoomData
+  bathrooms?: RoomData
+}
+
+export interface Property {
+  id: string;
+  address: string;
+  data: Data;
 }

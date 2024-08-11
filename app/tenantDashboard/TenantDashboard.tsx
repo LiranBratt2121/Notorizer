e3773a46dba11aceb0400609f7568672e8dd6986
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-12 02:06:16 +0300

    Add tenant dashboard component

---
+++ app/tenantDashboard/TenantDashboard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TenantDashboard = () => {
+  return (
+    <View>
+      <Text>TenantDashboard</Text>
+    </View>
+  )
+}
+
+export default TenantDashboard

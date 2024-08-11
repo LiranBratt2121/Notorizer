8f688288abc68a1578687a7eca3b63e4a6d3c9df
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-12 01:21:06 +0300

    Add tenant control buttons in index

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { View } from "react-native";
 import { router } from "expo-router";
 import React from "react";
@@ -7,9 +8,10 @@ import Button from "../components/common/Button";
 const Index = () => {
   return (
     <View>
-      <Button onPress={() => router.push("/account/SignIn")} title="Sign In" />
-
-      <Button onPress={() => router.push("/account/SignUp")} title="Sign Up" />
+      <Button onPress={() => router.push("/account/SignIn")} title="Sign In Landlord" />
+      <Button onPress={() => router.push("/account/SignUp")} title="Sign Up Landlord" />
+      <Button onPress={() => router.push("/tenantDashboard/TenantOTP")} title="Apply OTP Tenant" />
+      <Button onPress={() => router.push("/tenantDashboard/TenantLogin")} title="Sign In Tenant" />
     </View>
   );
 };

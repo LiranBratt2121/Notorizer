38397be2f9387e6a608e3402834b3cb03f3e2af3
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-11 22:59:32 +0300

    Add a function that checks if user with the same OTP exists

--- utils/FirebaseUtils.ts
+++ utils/FirebaseUtils.ts
@@ -1,5 +1,5 @@
 import { db } from "@/firebase/FirebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs} from "firebase/firestore";
 
 export const findDocumentIdByName = async (collectionPath: string, name: string) => {
   const querySnapshot = await getDocs(collection(db, collectionPath));
@@ -11,4 +11,29 @@ export const findDocumentIdByName = async (collectionPath: string, name: string)
       }
   });
   return docId;
+};
+
+export const findTenantByOTP = async (tenantName: string, otp: string) => {
+  try {
+    const tenantDocRef = doc(db, `tenantUser/${tenantName.trim()}`);
+    const tenantDocSnap = await getDoc(tenantDocRef);
+
+    if (tenantDocSnap.exists()) {
+      const tenantData = tenantDocSnap.data().tenantInfo;
+      console.log(tenantData);
+      if (tenantData && tenantData.otp === otp) {
+        console.log("Match found:", tenantDocSnap.id, tenantData);
+        return tenantData; // Return the matching document data
+      } else {
+        console.log("No matching tenant found with the given OTP.");
+        return null;
+      }
+    } else {
+      console.log("No such document!");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error finding tenant by OTP:", error);
+    return null;
+  }
 };

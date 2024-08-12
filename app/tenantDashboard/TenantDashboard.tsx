5c6954026e95e7c53e6afdba52b8c40e32d90ad5
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-12 05:24:33 +0300

    Add basic tenant dashboard

--- app/tenantDashboard/TenantDashboard.tsx
+++ app/tenantDashboard/TenantDashboard.tsx
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRouter } from 'expo-router'
+import Button from '@/components/common/Button'
 
 const TenantDashboard = () => {
+  const router = useRouter();
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>TenantDashboard</Text>
-    </View>
+      <Button 
+        title='Report Problem'
+        onPress={() => router.push({pathname:'/tenantDashboard/ReportProblem' as any})}
+      />
+
+      <Button 
+        title='Update Spaces'
+        onPress={() => router.push({pathname:'/tenantDashboard/UpdateSpaces' as any})}
+      />
+    </SafeAreaView>
   )
 }
 
